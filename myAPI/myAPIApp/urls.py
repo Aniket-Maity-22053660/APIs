656de2370b85a57bb69dd0e53f090e18c74ea1f6
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-06-20 07:02:10 +0000

    Committed!

--- myAPI/myAPIApp/urls.py
+++ myAPI/myAPIApp/urls.py
@@ -2,6 +2,30 @@ from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register('books', views.MyBooksView, basename="books")
-urlpatterns = router.urls
+#router = DefaultRouter()
+#router.register('books', views.MyBooksView, basename="books")
+#urlpatterns = router.urls,
+
+'''
+view1 = views.MyBooksView.as_view({
+    'get' : 'list',
+    'post' : 'create'
+})
+
+view2 = views.MyBooksView.as_view({
+    'get' : 'retrieve',
+    'put' : 'update',
+    'patch' : 'partial_update',
+    'delete' : 'destroy'
+})
+
+urlpatterns = [
+    path('books', view1, name="books"),
+    path('books/<int:pk>', view2, name="books-id")
+]
+'''
+
+urlpatterns = [
+    path('books/', views.MyBooksViewAll.as_view(), name="books"),
+    path('books/<int:pk>', views.MyBooksViewDetail.as_view(), name="books-pk")
+]
--- myAPI/myAPIApp/views.py
+++ myAPI/myAPIApp/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from . import models
 from . import serializer
+from rest_framework import generics
 # Create your views here.
 
 
-class MyBooksView(ModelViewSet):
+#class MyBooksView(ModelViewSet):
+#    queryset = models.MyBooks.objects.all()
+#    serializer_class = serializer.MyBooksSerializer
+
+class MyBooksViewAll(generics.ListCreateAPIView):
+    queryset = models.MyBooks.objects.all()
+    serializer_class = serializer.MyBooksSerializer
+class MyBooksViewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.MyBooks.objects.all()
     serializer_class = serializer.MyBooksSerializer
