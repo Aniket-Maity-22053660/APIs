25f9558f403bf0b85993c983d078c0fc987e4fae
Aniket-Maity-22053660 <22053660@kiit.ac.in>
2025-06-20 08:53:22 +0000

    Committed!

--- myAPI/myAPIApp/serializer.py
+++ myAPI/myAPIApp/serializer.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from . import models
+from decimal import Decimal
 class MyBooksSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source="inventory")
+    calculated_price = serializers.SerializerMethodField(method_name='calculate_tax')
     class Meta:
+        
         model = models.MyBooks
-        fields = "__all__"
+        fields =['id', 'title', 'price', 'stock', 'calculated_price', 'category']
+    def calculate_tax(self, product: models.MyBooks):
+        new_price = product.price + product.price*Decimal(0.5)
+        return (new_price)
